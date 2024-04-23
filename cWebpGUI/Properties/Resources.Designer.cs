ff84f8824a564de29419266f5df192a1c232cf7b
ThioJoe <12518330+ThioJoe@users.noreply.github.com>
2024-04-23 13:23:13 -0700

    Update Assembly Info names

--- cWebpGUI/Properties/Resources.Designer.cs
+++ cWebpGUI/Properties/Resources.Designer.cs
@@ -59,5 +59,14 @@ namespace cWebpGUI.Properties {
                 resourceCulture = value;
             }
         }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to 1.0.0.
+        /// </summary>
+        internal static string Version {
+            get {
+                return ResourceManager.GetString("Version", resourceCulture);
+            }
+        }
     }
 }
